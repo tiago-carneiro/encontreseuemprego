37ec6a69e249fc6a1474849de83707b2731ab578
Tiago Carneiro <tiago@phoenixsoftware.ca>
2024-11-03 03:41:20 +0000

    updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 (function ($) {
     "use strict";
+/* 
+const text = "Natalia Nobre  https://www.linkedin.com/in/nataliasnobre/";
+const regex = /(.+?)\s+(https?:\/\/\S+)/;
+const match = text.match(regex);
 
+if (match) {
+    const name = match[1];
+    const link = match[2];
+    console.log("Nome:", name);
+    console.log("Link:", link);
+} else {
+    console.log("Nenhum match encontrado.");
+}
+*/
     // Spinner
     var spinner = function () {
         setTimeout(function () {
@@ -85,6 +98,65 @@
             }
         }
     });
+    var items = 0;
+    var loadJobItems = function(page) {
+        $.ajax({
+            url: `https://my-json-server.typicode.com/tiago-carneiro/encontreseuemprego?_page=${page}&_limit=6`,
+            method: 'GET',
+            dataType: 'json',
+            success: function(jobList) {
+                createJobItems(jobList);
+                items = jobList.length + items;
+                
+                if(items == 16)
+                    $('#verMaisVagas').hide();
+            }
+        });
+    };
+
+    function createJobItems(jobList) {
+        const $container = $('#job-container');
+    
+        $.each(jobList, function(index, job) {
+            const $jobItem = $(`
+                <div class="job-item p-4 mb-4">
+                    <div class="row g-4">
+                        <div class="col-sm-12 col-md-8 d-flex align-items-center">
+                            <div class="text-start ps-4">
+                                <h5 class="mb-3">${job.titulo}</h5>
+                                ${job.empresa ? `<span class="text-truncate me-3"><i class="fa fa-building text-primary me-2"></i>${job.empresa}</span>` : ''}
+                                <span class="text-truncate me-3"><i class="fa fa-globe text-primary me-2"></i>${job.abrangencia == 'N' ? 'Nacional' : 'Internacional'}</span>
+                                 ${job.local ? `<span class="text-truncate me-3"><i class="fa fa-map-marker-alt text-primary me-2"></i>${job.local}</span>` : ''}
+                                ${createFormato(job.formato)}
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center">
+                            <div class="d-flex mb-3">
+                                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Detalhes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `);
+    
+            $container.append($jobItem);
+        });
+    }
+
+    var createFormato = function(value)
+    {
+        switch(value) {
+            case 'P':
+                return '<span class="text-truncate me-3"><i class="fa fa-building-user text-primary me-2"></i>Presencial</span>';
+            case 'R':
+                return '<span class="text-truncate me-3"><i class="fa fa-house-laptop text-primary me-2"></i>Remoto</span>';
+            case 'H':
+                return '<span class="text-truncate me-3"><i class="fa fa-house-circle-check text-primary me-2"></i>Híbrido</span>';
+            default:
+                return '';
+          } 
+    }
+    
+    loadJobItems(0);
     
 })(jQuery);
-
